a441161b1e76e84d72cbf7fa8c83f861ad48f411
Bodi_Ali <bodiali59@gmail.com>
2024-03-09 20:54:49 +0200

    Finish styling. Add all assets

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import "./style.css";
 import linktag from "./linktag.js";
+
 linktag();
 
 async function getWeather() {
   const response = await fetch(
-    "http://api.weatherapi.com/v1/current.json?key=4062a6c99201445190e72941240103&q=London"
+    "http://api.weatherapi.com/v1/forecast.json?days=4&key=4062a6c99201445190e72941240103&q=cairo"
   );
   const data = await response.json();
   console.log(data);
--- webpack.common.js
+++ webpack.common.js
@@ -24,6 +24,9 @@ module.exports = {
       {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
         type: "asset/resource",
+        generator: {
+          filename: "images/[name][ext]",
+        },
       },
       {
         test: /\.(woff|woff2|eot|ttf|otf|)$/i,
@@ -32,6 +35,10 @@ module.exports = {
       {
         test: /\.(mp4|webm|ogg|mov)$/i,
         type: "asset/resource",
+      },
+      {
+        test: /\.html$/i,
+        loader: "html-loader",
       },
     ],
   },
